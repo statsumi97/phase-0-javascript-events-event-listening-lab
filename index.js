9af6922858793c5622c8194bb07d4ca1fceaf67a
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-10-23 14:31:57 -0400

    final product

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
+  }
+  
+  addingEventListener(); // Call the function to attach the event listener  
